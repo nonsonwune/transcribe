81643299268c1745ce801ea778810a2bf301d745
Nonso Nwune <nonsonwune@Nonsos-MacBook-Pro.local>
2024-09-12 06:17:22 +0100

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded", function () {
-  const body = document.body;
-  const darkModeToggle = document.getElementById("darkModeToggle");
-  const uploadAudioButton = document.getElementById("uploadAudio");
-  const fileInput = document.getElementById("fileInput");
-  const uploadForm = document.getElementById("uploadForm");
-  const uploadMessage = document.getElementById("uploadMessage");
-  const fileList = document.getElementById("fileList");
-  const transcribeFilesButton = document.getElementById("transcribeFiles");
-  const downloadFilesButton = document.getElementById("downloadFiles");
-
-  darkModeToggle.addEventListener("change", function () {
-    body.classList.toggle("dark-mode", this.checked);
-    console.log(this.checked ? "Dark mode enabled" : "Dark mode disabled");
-=======
 $(document).ready(function () {
   // Check the session for dark mode preference
   $.ajax({
@@ -28,7 +12,6 @@ $(document).ready(function () {
         $("body").removeClass("dark-mode");
       }
     },
->>>>>>> sankofa
   });
 
   $("#darkModeToggle").on("change", function () {
@@ -46,15 +29,6 @@ $(document).ready(function () {
     });
   });
 
-<<<<<<< HEAD
-  fileInput.addEventListener("change", function (event) {
-    if (fileInput.files.length > 0) {
-      updateFileList(fileInput.files);
-      uploadMessage.textContent = "Files selected, ready to upload.";
-      uploadMessage.className = "alert alert-info";
-      uploadMessage.style.display = "block";
-      transcribeFilesButton.style.display = "block"; // Show the transcribe button
-=======
   let files;
   let sessionId;
 
@@ -69,7 +43,6 @@ $(document).ready(function () {
     if (!files) {
       alert("Please select a file first.");
       return;
->>>>>>> sankofa
     }
 
     let formData = new FormData();
@@ -114,69 +87,6 @@ $(document).ready(function () {
             $(".lds-circle").hide(); // Hide the spinner on error
           },
         });
-<<<<<<< HEAD
-      })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Upload failed: ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(() => {
-        uploadMessage.textContent =
-          "Files uploaded successfully. Ready to transcribe.";
-        uploadMessage.className = "alert alert-success";
-        uploadMessage.style.display = "block";
-        transcribeFilesButton.style.display = "block"; // Show the transcribe button
-      })
-      .catch((error) => {
-        console.error(error);
-        uploadMessage.textContent = "Upload failed.";
-        uploadMessage.className = "alert alert-danger";
-        uploadMessage.style.display = "block";
-      });
-  });
-
-  transcribeFilesButton.addEventListener("click", function () {
-    transcribeFilesButton.disabled = true;
-    uploadMessage.textContent = "Transcription in progress...";
-    uploadMessage.className = "alert alert-info";
-    uploadMessage.style.display = "block";
-
-    fetch("/process")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Transcription failed: ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(() => {
-        uploadMessage.textContent = "Transcription completed.";
-        uploadMessage.className = "alert alert-success";
-        uploadMessage.style.display = "block";
-        downloadFilesButton.style.display = "block"; // Show the download button
-      })
-      .catch((error) => {
-        console.error(error);
-        uploadMessage.textContent = "Transcription failed.";
-        uploadMessage.className = "alert alert-danger";
-        uploadMessage.style.display = "block";
-        transcribeFilesButton.disabled = false;
-      });
-  });
-
-  downloadFilesButton.addEventListener("click", function () {
-    window.location.href = "/download";
-  });
-
-  function updateFileList(files) {
-    fileList.innerHTML = "";
-    Array.from(files).forEach((file) => {
-      const listItem = document.createElement("li");
-      listItem.className = "list-group-item";
-      listItem.textContent = file.name;
-      fileList.appendChild(listItem);
-=======
       },
     });
   });
@@ -199,7 +109,6 @@ $(document).ready(function () {
         $("#transcribeButton").prop("disabled", false);
         $(".lds-circle").hide(); // Hide the spinner on error
       },
->>>>>>> sankofa
     });
   }
 
